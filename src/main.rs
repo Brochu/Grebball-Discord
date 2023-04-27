94e2b8a35565a7bc5e5e8c876042ca57410a3de3
Brochu <alexbrochu1@gmail.com>
2023-04-27 01:57:41 -0400

    [DEV] Implemented very simple ping pong bot for first version

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
 use dotenv::dotenv;
-//use std::env;
+use std::env;
 
-fn main() {
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+
+            if let Err(reason) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("[EVT] Error sending response: {:?}", reason);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
     dotenv().ok();
+    let token = env::var("DISCORD_TOKEN").expect("[ENV] Need to set DISCORD_TOKEN env var.");
 
-    println!("Hello, world!");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("[CLIENT] Could not create client.");
+
+    if let Err(reason) = client.start().await {
+        println!("[MAIN] Client error: {:?}", reason);
+    }
 }
