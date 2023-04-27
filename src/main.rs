39e165e6e009957f54892285eff2d3bcb93872b7
Brochu <alexbrochu1@gmail.com>
2023-04-27 12:36:20 -0400

    [DEV] Started workon getting slash commands to work with the bot
    I need to setup Shuttle to host discord bot code in order to test slash
    commands properly

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,52 @@ use dotenv::dotenv;
 use std::env;
 
 use serenity::async_trait;
-use serenity::model::channel::Message;
+use serenity::model::application::interaction::{ Interaction, InteractionResponseType };
 use serenity::model::gateway::Ready;
+use serenity::model::id::GuildId;
 use serenity::prelude::*;
+
+mod commands;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(cmd) = interaction {
+            println!("[EVT] Got command interaction: {:#?}", cmd);
 
-            if let Err(reason) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("[EVT] Error sending response: {:?}", reason);
-            }
-        }
+            let content = match cmd.data.name.as_str() {
+                "ping" => commands::ping::run(&cmd.data.options),
+                _ => "[EVT] Command not implemented".to_string()
+            };
 
-        if msg.content.starts_with("!week") {
-            let (_, arg) = msg.content.split_once(" ").expect("[EVT] Could not split");
-
-            if let Err(reason) = msg.channel_id.say(&ctx.http, arg).await {
-                println!("[EVT] Error sending response: {:?}", reason);
+            if let Err(reason) = cmd.create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|msg| msg.content(content))
+                }).await
+            {
+                println!("[EVT] Cannot reply to slash command: {}", reason);
             }
         }
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(env::var("GUILD_ID")
+            .expect("[EVT] Could not find GUILD_ID env var")
+            .parse()
+            .expect("[EVT] GUILD_ID needs to be an integer")
+        );
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |cmds| {
+            cmds
+                .create_application_command(|cmd| commands::ping::register(cmd))
+        }).await;
+
+        println!("[EVT] I now have access to the following slash commands: {:#?}", commands);
     }
 }
 
@@ -37,11 +56,7 @@ async fn main() {
     dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("[ENV] Need to set DISCORD_TOKEN env var.");
 
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
-
-    let mut client = Client::builder(&token, intents)
+    let mut client = Client::builder(&token, GatewayIntents::empty())
         .event_handler(Handler)
         .await
         .expect("[CLIENT] Could not create client.");
