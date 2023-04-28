eb732c19e53f923c2772699bba98ee2ae31c2c2c
Brochu <alexbrochu1@gmail.com>
2023-04-28 16:35:10 -0400

    [DEV] Brought back the simple ping bot online with Serenity only
    Next step: Handle slash commands and try to get matches for one week

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use dotenv::dotenv;
+use std::env;
+
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
-use serenity::model::id::GuildId;
-use serenity::model::prelude::interaction::{ Interaction, InteractionResponseType };
+//use serenity::model::id::GuildId;
+//use serenity::model::prelude::interaction::{ Interaction, InteractionResponseType };
 use serenity::prelude::*;
 
 mod commands;
@@ -12,15 +15,41 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(reason) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("![Handler] Handler message error : {:?}", reason);
+            }
+        }
     }
 
     //TODO: Handle slash commands
     //async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
     //}
 
-    async fn ready(&self, ctx: Context, ready: Ready) {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("[Handler] ({}) {} is connected w/ version: {}",
+            ready.user.id,
+            ready.user.name,
+            ready.version
+        );
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    dotenv().ok(); // Include .env file to environment
+
+    let token = env::var("DISCORD_TOKEN").expect("[MAIN] Cannot find 'DISCORD_TOKEN' in env");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("[MAIN] Could not create client");
+
+    if let Err(reason) = client.start().await {
+        println!("![MAIN] Client error : {:?}", reason);
+    }
 }
