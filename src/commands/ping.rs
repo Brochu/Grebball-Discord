4b3b406461adef6c866387be86826bb2f87e7a63
Brochu <alexbrochu1@gmail.com>
2023-04-30 12:08:56 -0400

    [DEV] Re-implemented simple ping app command, that now works

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,11 +1,12 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
 
-pub fn _run(_options: &[CommandDataOption]) -> String {
-    return "".to_string();
+pub fn run(_options: &[CommandDataOption]) -> String {
+    return "Hey, this bot is online!".to_string();
 }
 
-pub fn _register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command.name("ping")
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command
+        .name("ping")
         .description("A ping command")
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::env;
 use serde_json::Value;
 
 use serenity::async_trait;
+use serenity::model::application::interaction::{ Interaction, InteractionResponseType };
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
-//use serenity::model::prelude::interaction::{ Interaction, InteractionResponseType };
 use serenity::prelude::*;
 
 mod commands;
@@ -65,23 +65,44 @@ impl EventHandler for Handler {
         }
     }
 
-    //TODO: Handle slash commands
-    //async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-    //}
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(cmd) = interaction {
+            println!("[Handler] Got command interaction: {:#?}", cmd);
 
-    async fn ready(&self, _: Context, ready: Ready) {
+            let reply = match cmd.data.name.as_str() {
+                "ping" => commands::ping::run(&cmd.data.options),
+                _ => "Command not implemented!".to_string(),
+            };
+
+            if let Err(reason) = cmd.create_interaction_response(&ctx.http, |res| {
+                res
+                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                    .interaction_response_data(|m| m.content(reply))
+            })
+            .await {
+                println!("![Handler] Cannot respond to slash command : {:?}", reason);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("[Handler] ({}) {} is connected w/ version: {}",
             ready.user.id,
             ready.user.name,
             ready.version
         );
 
-        //TODO: Create the commands needed
         let guild_id = GuildId(env::var("GUILD_ID")
             .expect("![Handler] Could not find env var 'GUILD_ID'")
             .parse()
             .expect("![Handler] Could not parse guild_id to int")
         );
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |cmds| {
+            cmds.create_application_command(|cmd| commands::ping::register(cmd))
+        }).await;
+
+        println!("Here are the available commands: {:#?}", commands);
     }
 }
 
