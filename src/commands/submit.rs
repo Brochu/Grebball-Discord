d17260d662bfa3770d8e2a7dbddff2086464db51
Brochu <alexbrochu1@gmail.com>
2023-05-03 01:18:11 -0400

    [NOTES] Adding notes for next step
    Figure out if we can send some sort of metadata with the week message to
    know what season/week we are saving to DB. Also need to track which
    pooler is picking

--- src/commands/submit.rs
+++ src/commands/submit.rs
@@ -1,11 +1,11 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
 use serenity::model::prelude::{MessageId, ReactionType};
-use serenity::model::prelude::interaction::InteractionResponseType;
+//use serenity::model::prelude::interaction::InteractionResponseType;
 use serenity::model::prelude::command::CommandType;
 use serenity::prelude::*;
 
-pub async fn run(ctx: Context, command: &ApplicationCommandInteraction) {
+pub async fn run(_: Context, command: &ApplicationCommandInteraction) {
     println!("Data sent with the submit command : {:#?}", command.data);
 
     if let Some(id) = command.data.target_id {
@@ -23,24 +23,27 @@ pub async fn run(ctx: Context, command: &ApplicationCommandInteraction) {
 
         message.reactions.iter().for_each(|r| {
             if let ReactionType::Custom { name, .. } = &r.reaction_type {
-                println!("reaction: {:#?}", name);
+                let pick = name.as_ref().unwrap();
+                println!("reaction: {:#?}", pick);
             }
         });
 
-        if let Err(reason) = command.create_interaction_response(&ctx.http, |res| {
-            res
-                .kind(InteractionResponseType::ChannelMessageWithSource)
-                .interaction_response_data(|m| m
-                    .content("Found the message with picks to submit")
-                    //TODO: Look for more options here
-                )
-        })
-        .await {
-            println!("![week] Cannot respond to slash command : {:?}", reason);
-        }
-    }
+        //TODO: How can we track match ids, season and week number to add to db
 
-    println!("![Submit] No target message sent with the command");
+        //TODO: Maybe send a message in case of error later
+        // Handle adding picks to database first
+        //if let Err(reason) = command.create_interaction_response(&ctx.http, |res| {
+        //    res
+        //        .kind(InteractionResponseType::ChannelMessageWithSource)
+        //        .interaction_response_data(|m| m
+        //            .content("Found the message with picks to submit")
+        //            //TODO: Look for more options here
+        //        )
+        //})
+        //.await {
+        //    println!("![week] Cannot respond to slash command : {:?}", reason);
+        //}
+    }
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
