7d12d2bf0774d772fe9f8b2c33886f505f6829b4
Brochu <alexbrochu1@gmail.com>
2023-05-05 11:48:40 -0400

    [DEV] Use new database part of the library

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,27 @@
 use std::env;
-use mongodb::{ bson::doc, options::ClientOptions, Client };
+use std::fmt::Display;
+
+use mongodb::{ bson::oid::ObjectId, bson::doc, options::ClientOptions, Client };
+
+pub struct PoolerResult {
+    pooler_id: ObjectId,
+    pooler_name: String,
+
+    match_ids: Vec<String>,
+    results: Vec<i8>,
+}
+
+impl Display for PoolerResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut out = format!("[{}] {}\n", self.pooler_id, self.pooler_name);
+
+        self.match_ids.iter().enumerate().for_each(|(idx, mid)| {
+            out.push_str(format!("\t{}: {}\n", mid, self.results[idx]).as_str());
+        });
+
+        writeln!(f, "{out}")
+    }
+}
 
 pub async fn ping() {
     let uri = env::var("MONGDO_URI")
@@ -13,4 +35,16 @@ pub async fn ping() {
         .run_command(doc! { "ping": 1 }, None).await.unwrap();
 
     println!("Ping result: {:#?}", result);
+}
+
+pub async fn fetch_results() -> Option<impl Iterator<Item=PoolerResult>> {
+    //TODO: Need to find the actual data, mocked for now
+    let temp = vec![ PoolerResult {
+        pooler_id: ObjectId::new(),
+        pooler_name: "".to_string(),
+        match_ids: vec![],
+        results: vec![]
+    }];
+
+    Some(temp.into_iter())
 }
