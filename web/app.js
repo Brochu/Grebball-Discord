542a0b8a698dee471e8dd8834132aca258a4b884
Brochu <alexbrochu1@gmail.com>
2023-05-20 01:44:47 -0400

    [DEV] Showing team logo for picks in radion button labels

--- web/app.js
+++ web/app.js
@@ -17,12 +17,58 @@ app.get('/:season/:week/:token', async (req, res) => {
 
     const result = await fetch(url);
     const json = await result.json();
-    const events = json['events']
+    const matches = json['events'].map((m) => {
+        m['awayTeam'] = GetTeamShortName(m['strAwayTeam']);
+        m['homeTeam'] = GetTeamShortName(m['strHomeTeam']);
+        return m;
+    });
 
-    res.render('picks.html', { season, week, matches: events });
+    res.render('picks.html', { season, week, matches });
 });
 
 app.listen(port, () => {
     console.log(`using -${__dirname + '\\node_modules\\bootstrap\\dist'}- for static files`);
     console.log(`Picks page application, listening on port ${port}`);
 });
+
+const lNameMap = {
+    'Arizona Cardinals'    : 'ARI',
+    'Atlanta Falcons'      : 'ATL',
+    'Baltimore Ravens'     : 'BAL',
+    'Buffalo Bills'        : 'BUF',
+    'Carolina Panthers'    : 'CAR',
+    'Chicago Bears'        : 'CHI',
+    'Cincinnati Bengals'   : 'CIN',
+    'Cleveland Browns'     : 'CLE',
+    'Dallas Cowboys'       : 'DAL',
+    'Denver Broncos'       : 'DEN',
+    'Detroit Lions'        : 'DET',
+    'Green Bay Packers'    : 'GB',
+    'Houston Texans'       : 'HOU',
+    'Indianapolis Colts'   : 'IND',
+    'Jacksonville Jaguars' : 'JAX',
+    'Kansas City Chiefs'   : 'KC',
+    'Los Angeles Rams'     : 'LA',
+    'Los Angeles Chargers' : 'LAC',
+    'Las Vegas Raiders'    : 'LV',
+    'Oakland Raiders'      : 'LV',
+    'Miami Dolphins'       : 'MIA',
+    'Minnesota Vikings'    : 'MIN',
+    'New England Patriots' : 'NE',
+    'New Orleans Saints'   : 'NO',
+    'New York Giants'      : 'NYG',
+    'New York Jets'        : 'NYJ',
+    'Philadelphia Eagles'  : 'PHI',
+    'Pittsburgh Steelers'  : 'PIT',
+    'Seattle Seahawks'     : 'SEA',
+    'San Francisco 49ers'  : 'SF',
+    'Tampa Bay Buccaneers' : 'TB',
+    'Tennessee Titans'     : 'TEN',
+    'Washington'           : 'WAS',
+    'Washington Commanders': 'WAS',
+    'Washington Redskins'  : 'WAS'
+};
+
+const GetTeamShortName = (longname) => {
+    return lNameMap[longname];
+};
