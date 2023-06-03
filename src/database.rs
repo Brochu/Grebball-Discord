976189cb9a5c4c85b72f6e117c224f135149631d
Brochu <alexbrochu1@gmail.com>
2023-06-03 02:07:01 -0400

    [FMT] SQL queries were badly formatted

--- src/database.rs
+++ src/database.rs
@@ -21,13 +21,21 @@ impl DB {
         println!("[DB] Getting results for discordid: {discordid}, week {week}");
         //TODO: Finish implementation
 
-        let users = sqlx::query("SELECT id, email, discordid FROM Users")
+        let users = sqlx::query("
+                SELECT id, season, week, pickstring, poolerid, scorecache FROM picks
+                ")
             .fetch_all(&self.pool)
             .await?;
 
         for row in users {
-            let email = row.get::<String, &str>("email");
-            println!("{}", email);
+            let id: i64 = row.get("id");
+            let season: i64 = row.get("season");
+            let week: i64 = row.get("week");
+            let picks: Option<String> = row.get("pickstring");
+            let poolerid: i64 = row.get("poolerid");
+            let score: Option<i64> = row.get("scorecache");
+
+            println!("|{}|{}|{}|{:?}|{}|{:?}|", id, season, week, picks, poolerid, score);
         }
 
         Ok(())
@@ -50,39 +58,39 @@ impl DB {
             .get("id");
 
         match sqlx::query("
-            SELECT id, pickstring FROM picks
-            WHERE poolerid = ? AND season = ? AND week = ?
-            ")
-        .bind(poolerid)
-        .bind(season)
-        .bind(week)
-        .fetch_one(&self.pool)
-        .await {
-            Ok(row) => {
-                if let Some(_) = row.get::<Option<String>, &str>("pickstring") {
-                    return Err(anyhow!("[DB] Picks for given pooler, season and week already entered!"));
-                }
-                else {
-                    // Picks are already primed and not filled
-                    let id: i64 = row.get("id");
-                    return Ok(id);
-                }
-            },
-            Err(_) => {
-                let new_row = sqlx::query("
-                        INSERT INTO picks (season, week, poolerid)
-                        VALUES (?, ?, ?);
-                        SELECT last_insert_rowid();
-                        ")
-                    .bind(season)
-                    .bind(week)
-                    .bind(poolerid)
-                    .fetch_one(&self.pool)
-                    .await?;
+                SELECT id, pickstring FROM picks
+                WHERE poolerid = ? AND season = ? AND week = ?
+                ")
+            .bind(poolerid)
+            .bind(season)
+            .bind(week)
+            .fetch_one(&self.pool)
+            .await {
+                Ok(row) => {
+                    if let Some(_) = row.get::<Option<String>, &str>("pickstring") {
+                        return Err(anyhow!("[DB] Picks for given pooler, season and week already entered!"));
+                    }
+                    else {
+                        // Picks are already primed and not filled
+                        let id: i64 = row.get("id");
+                        return Ok(id);
+                    }
+                },
+                Err(_) => {
+                    let new_row = sqlx::query("
+                            INSERT INTO picks (season, week, poolerid)
+                            VALUES (?, ?, ?);
+                            SELECT last_insert_rowid();
+                            ")
+                        .bind(season)
+                        .bind(week)
+                        .bind(poolerid)
+                        .fetch_one(&self.pool)
+                        .await?;
 
-                let id: i64 = new_row.get(0);
-                Ok(id)
-            }
+                    let id: i64 = new_row.get(0);
+                    Ok(id)
+                }
         }
     }
 }
