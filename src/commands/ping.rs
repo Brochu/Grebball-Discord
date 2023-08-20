875ce11b71bce78ff4d483069618ffd93158f476
Brochu <alexbrochu1@gmail.com>
2023-08-19 22:51:54 -0400

    Deactivate unused commands for now

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,23 +1,23 @@
-use serenity::builder::CreateApplicationCommand;
-use serenity::model::application::interaction::InteractionResponseType;
-use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
-use serenity::prelude::*;
-
-pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command
-        .name("ping")
-        .description("A ping command")
-}
-
-pub async fn run(ctx: Context, command: &ApplicationCommandInteraction) {
-    if let Err(reason) = command.create_interaction_response(&ctx.http, |res| {
-        res
-            .kind(InteractionResponseType::ChannelMessageWithSource)
-            .interaction_response_data(|m| m
-                .content("Ping response!")
-            )
-    })
-    .await {
-        println!("![ping] Cannot respond to slash command : {:?}", reason);
-    }
-}
+//use serenity::builder::CreateApplicationCommand;
+//use serenity::model::application::interaction::InteractionResponseType;
+//use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
+//use serenity::prelude::*;
+//
+//pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+//    command
+//        .name("ping")
+//        .description("A ping command")
+//}
+//
+//pub async fn run(ctx: Context, command: &ApplicationCommandInteraction) {
+//    if let Err(reason) = command.create_interaction_response(&ctx.http, |res| {
+//        res
+//            .kind(InteractionResponseType::ChannelMessageWithSource)
+//            .interaction_response_data(|m| m
+//                .content("Ping response!")
+//            )
+//    })
+//    .await {
+//        println!("![ping] Cannot respond to slash command : {:?}", reason);
+//    }
+//}
--- src/main.rs
+++ src/main.rs
@@ -39,9 +39,9 @@ impl EventHandler for Bot {
             match cmd.data.name.as_str() {
                 "matches" => commands::matches::run(ctx, &cmd).await,
                 "picks"   => commands::picks::run(ctx, &cmd, &self.database).await,
-                "ping"    => commands::ping::run(ctx, &cmd).await,
+                //"ping"    => commands::ping::run(ctx, &cmd).await,
                 "results" => commands::results::run(ctx, &cmd, &self.database).await,
-                "season" => commands::season::run(ctx, &cmd, &self.database).await,
+                //"season" => commands::season::run(ctx, &cmd, &self.database).await,
                 _         => println!("![Handler] Command not implemented!"),
             };
         }
@@ -64,9 +64,9 @@ impl EventHandler for Bot {
             cmds
                 .create_application_command(|cmd| commands::matches::register(cmd))
                 .create_application_command(|cmd| commands::picks::register(cmd))
-                .create_application_command(|cmd| commands::ping::register(cmd))
+                //.create_application_command(|cmd| commands::ping::register(cmd))
                 .create_application_command(|cmd| commands::results::register(cmd))
-                .create_application_command(|cmd| commands::season::register(cmd))
+                //.create_application_command(|cmd| commands::season::register(cmd))
         }).await.expect("![Handler] Could not set application commands in Discord Guild");
 
         println!("[Handler] Here are the available commands:");
